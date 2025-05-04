f6e8f6b1038f4ed16812abb6671846bddd153683
Tuggyston <recreationalgaming33@gmail.com>
2025-05-04 15:59:28 -0500

    Security added

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://vincentpiercedev.github.io/CleanLivingWithShannon',
   base: '/CleanLivingWithShannon',
+  output: 'static',
+  compressHTML: true,
   integrations: [react()],
+  adapter: netlify(),
 });
