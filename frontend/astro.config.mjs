ecaa003deb1c3fc29b6ee8d691366dfdefb90a4e
Tuggyston <recreationalgaming33@gmail.com>
2025-05-05 19:06:57 -0500

    updated the site to http temporarily

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://cleanlivingwithshannon.com',
+  site: 'http://cleanlivingwithshannon.com',
   base: '/',
   output: 'static',
   compressHTML: true,
