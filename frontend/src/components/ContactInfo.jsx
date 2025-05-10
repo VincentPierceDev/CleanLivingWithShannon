865e4209590d4ca65ceec08a2a4a9a3efd70efc3
Tuggyston <recreationalgaming33@gmail.com>
2025-05-09 20:16:37 -0500

    removed logs

--- frontend/src/components/ContactInfo.jsx
+++ frontend/src/components/ContactInfo.jsx
@@ -11,10 +11,8 @@ const ContactInfo = (props) => {
     useEffect(() => {
         const updateState = () => setVerified(true);
         window.addEventListener("recaptcha-verified-success", updateState);
-        console.log("recaptcha bind");
 
         return () => {
-            console.log("recaptcha unbound");
             return window.removeEventListener("recaptcha-verified-success", updateState);
         }
     }, [])
